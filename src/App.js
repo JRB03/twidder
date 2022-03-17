aa0d5926e8eb7162062b50e901ae1758d2cbedf4
Jack <jrbas263@gmail.com>
2022-03-17 18:10:09 -0400

    added like button to tweet with animated heart

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import Exhibit from './Components/Exhibit.js';
 import Tweet from './Components/Tweet.js';
-import Header from './Components/Header';
-import '../Styles/App.css';
+import Header from './Components/Header.js';
+import './Styles/App.css';
 
 const App = () => {
   //hot tweeds
-  let ex1 = [<Tweet content="ya"/>,"yeet","ya"];
-
     return (
     <div>
       <Header/>
-      <Exhibit 
-        header={"Hot Tweeds"}
-        clist={ex1}
-      />
     </div>
   );
 }
--- src/Components/Tweet.js
+++ src/Components/Tweet.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, {useState} from 'react';
 import '../Styles/Tweet.css';
+import empty from '../Images/xheart.png';
+import loved from '../Images/oheart.png';
 
 const Tweet = props => {
+   const[Likes,setLikes] = useState(0);
+   const[Heart,setHeart] = useState(false);
+   const Like = () => { 
+      setHeart(!Heart);
+      Heart ? setLikes(Likes-1) : setLikes(Likes+1);
+   };
+
    return (
-      <div>
-         <p class="content">{props.content}</p>
-         <p class="user">{props.user}</p>
-         <p class="date">{props.date}</p>
+      <div class="tweet-container">
+         <p>hey</p>
+         <p>{props.content}</p>
+         <p>{props.user}</p>
+         <p>{props.date}</p>
+         <div onClick={Like} class="likes">
+            <img class="heart" src = {Heart ? loved : empty}></img>
+            <p>{Likes}</p>
+         </div>
+         
+         
       </div>
    );
 }
